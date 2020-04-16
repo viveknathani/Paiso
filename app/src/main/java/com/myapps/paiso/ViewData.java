a37e6b255bb13258f2d0500d3544c7f9b55cadc6
viveknathani <viveknathani2402@gmail.com>
2020-04-16 09:49:41 +0530

    functional state

--- app/src/main/java/com/myapps/paiso/ViewData.java
+++ app/src/main/java/com/myapps/paiso/ViewData.java
@@ -3,6 +3,7 @@ package com.myapps.paiso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,32 +17,35 @@ public class ViewData extends AppCompatActivity {
         setContentView(R.layout.activity_view_data);
         DatabaseHandler db=new DatabaseHandler(this);
         Map<Date, LinkedList<ExpenseData>> hashTable=db.returnHashTable();
+        LinearLayout ll_main=findViewById(R.id.main_view_data);
+        String multilineText="";
 
         for(Map.Entry<Date, LinkedList<ExpenseData>> entry : hashTable.entrySet())
         {
             String dateE=entry.getValue().get(0).getDate();
-            addTextView(dateE);
+            multilineText=multilineText+dateE+"\n";
+            Log.d("DATE", dateE);
             for(int i=0; i<entry.getValue().size(); i++)
             {
                 String nameE=entry.getValue().get(i).getExpenseName();
                 float amount=entry.getValue().get(i).getAmount();
-                String paymentMode=entry.getValue().get(i).getExpenseName();
-                addTextView(nameE);
-                addTextView(String.valueOf(amount));
-                addTextView(paymentMode);
+                String paymentMode=entry.getValue().get(i).getPaymentMode();
+                multilineText=multilineText+nameE+"\n"+amount+"\n"+paymentMode+"\n";
             }
         }
+
+        addTextView(ll_main, multilineText);
     }
 
-    protected void addTextView(String text){
-        LinearLayout ll_main=findViewById(R.id.main_view_data);
+    protected void addTextView(LinearLayout ll_main, String text){
         TextView textView = new TextView(this);
-        textView.setText(text);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                 LinearLayout.LayoutParams.MATCH_PARENT,
                 LinearLayout.LayoutParams.MATCH_PARENT
         );
+        ll_main.setOrientation(LinearLayout.VERTICAL);
         textView.setLayoutParams(params);
+        textView.setText(text);
         ll_main.addView(textView);
     }
 }
