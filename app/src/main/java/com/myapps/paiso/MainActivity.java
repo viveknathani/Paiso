86383a64f12e6d1ce83e6be835969dd4a7554d67
viveknathani <viveknathani2402@gmail.com>
2020-04-18 11:12:50 +0530

    intro UI

--- app/src/main/java/com/myapps/paiso/MainActivity.java
+++ app/src/main/java/com/myapps/paiso/MainActivity.java
@@ -1,36 +1,39 @@
 package com.myapps.paiso;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //activity appears only during the first run of the app
         SharedPreferences preferences=getSharedPreferences("PREFERENCE", MODE_PRIVATE);
         String firstTime=preferences.getString("FirstTime", "");
-
-        if(firstTime.equals("Yes")){
+        if(firstTime.equals("Yes"))
+        {
             Intent intent=new Intent(MainActivity.this, GetDetails.class);
             startActivity(intent);
         }
-        else{
+        else
+        {
             SharedPreferences.Editor editor=preferences.edit();
             editor.putString("FirstTime", "Yes");
             editor.apply();
         }
     }
 
-    public void onClickNext(View view){
+    public void onClickNext(View view)
+    {
         EditText nameView=(EditText)findViewById(R.id.name);
         String nameText=nameView.getText().toString();
         Intent intent=new Intent(this, GetDetails.class);
