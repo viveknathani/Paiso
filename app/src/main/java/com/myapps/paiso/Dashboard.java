28985cea993ad9e3814b9db775be2a555c7676d8
viveknathani <viveknathani2402@gmail.com>
2020-04-20 21:13:10 +0530

    stable display

--- app/src/main/java/com/myapps/paiso/Dashboard.java
+++ app/src/main/java/com/myapps/paiso/Dashboard.java
@@ -1,10 +1,20 @@
 package com.myapps.paiso;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dashboard extends AppCompatActivity
@@ -33,6 +43,42 @@ public class Dashboard extends AppCompatActivity
         db_details_0.setText("Bank : "+Float.toString(detailsList.get(0)));
         db_details_1.setText("Cash : "+Float.toString(detailsList.get(1)));
         db_details_2.setText("E-Wallet : "+Float.toString(detailsList.get(2)));
+
+        bank_details=detailsList.get(0);
+        cash_details=detailsList.get(1);
+        ewallet_details=detailsList.get(2);
+
+        float totalValue=bank_details+cash_details+ewallet_details;
+
+        float percent_bank_details=bank_details*100/totalValue;
+        float percent_cash_details=cash_details*100/totalValue;
+        float percent_ewallet_details=ewallet_details*100/totalValue;
+
+        PieChart chart=(PieChart)findViewById(R.id.pie_chart);
+
+        List<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(percent_bank_details, "Bank"));
+        entries.add(new PieEntry(percent_cash_details, "Cash"));
+        entries.add(new PieEntry(percent_ewallet_details, "E-Wallets"));
+
+
+        PieDataSet set = new PieDataSet(entries, null);
+        set.setColors(new int[]{Color.parseColor("#F3F56C"),
+                Color.parseColor("#6CF57D"),
+                Color.parseColor("#F56CA6")});
+        PieData data = new PieData(set);
+        data.setValueTextSize(30f);
+
+        chart.setCenterText("Paiso Distribution");
+        chart.setHoleColor(Color.WHITE);
+        chart.setCenterTextColor(Color.BLACK);
+        chart.setCenterTextSize(18);
+        chart.setUsePercentValues(true);
+        chart.setEntryLabelColor(R.color.mainDarkColor);
+        chart.getDescription().setEnabled(false);
+        chart.setDrawEntryLabels(false);
+        chart.setData(data);
+        chart.invalidate();
     }
 
     public void onClickAddActivity(View view)
